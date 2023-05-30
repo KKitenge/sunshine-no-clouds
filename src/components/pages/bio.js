90b0b658d393e16810d9e1421bf00a2968c85c9d
Ox Noj <geneveva@gmail.com>
2023-05-30 16:56:33 -0700

    Sigh 3 - still trying to get this right

--- src/components/pages/bio.js
+++ src/components/pages/bio.js
@@ -1,18 +1,20 @@
 import React from "react";
-import '../assets/css/style.css';
-import Footer from '../Footer'
+import "../assets/css/style.css";
+import stock from "../assets/images/stock.png";
+// import Header from "../Header";
 
 function Bio() {
-    return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">About Me</h5>
-                <p className="card-text">ABCD-EFG-HIJ-K_LMNO-P-QRS-TUV-WX-Y-Zed.</p>
-                <a href="#" className="btn btn-primary">Hmm</a>
-            </div>
+  return (
+    <div>
+      <div className="card">
+        <img src={stock} className="card-img-top"/>
+        <div className="card-body">
+          <h5 className="card-title">About Me</h5>
+          <p className="card-text">ABCD-EFG-HIJ-K_LMNO-P-QRS-TUV-WX-Y-Zed.</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Bio;
