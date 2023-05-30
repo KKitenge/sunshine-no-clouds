170456c7952a3706da23aed53606a1820b0e4f56
Ox Noj <geneveva@gmail.com>
2023-05-29 20:15:47 -0700

    Added additional files/working on pages.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import PortfolioContainer from './components/PortfolioContainer';
+
+function App() {
+  return (
+    <div className="portfolio">
+      <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
