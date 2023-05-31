1ca7fd55e67a174ea35cd284bfec96d68f6d7097
Ox Noj <geneveva@gmail.com>
2023-05-30 18:57:00 -0700

    Editing Contact page form

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,22 +1,173 @@
-import React from "react";
-import '../assets/css/style.css';
+// import React from "react";
+// import "../assets/css/style.css";
+
+// function Contact() {
+//     const [submitted, setSubmitted] = useState(false);
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         const formData = new FormData(e.target);
+//         const firstName = formData.get('first-name');
+//         const lastName = formData.get('last-name');
+//         const email = formData.get('email');
+//         const message = formData.get('message');
+
+//         setSubmitted(true);
+
+//     return (
+//         <form id="contact-form" action="#" method="post">
+//             <div className="mb-3">
+//                 <label htmlFor="first-name" className="form-label">
+//                     First Name
+//                 </label>
+//                 <input
+//                     type="text"
+//                     className="form-control"
+//                     id="first-name"
+//                     name="first-name"
+//                     required
+//                 />
+//                 <div className="invalid-feedback">Please enter your first name.</div>
+//             </div>
+
+//             <div className="mb-3">
+//                 <label htmlFor="last-name" className="form-label">
+//                     Last Name
+//                 </label>
+//                 <input
+//                     type="text"
+//                     className="form-control"
+//                     id="last-name"
+//                     name="last-name"
+//                     required
+//                 />
+//                 <div className="invalid-feedback">Please enter your last name.</div>
+//             </div>
+
+//             <div className="mb-3">
+//                 <label htmlFor="email" className="form-label">
+//                     Email Address
+//                 </label>
+//                 <input
+//                     type="email"
+//                     className="form-control"
+//                     id="email"
+//                     name="email"
+//                     required
+//                 />
+//                 <div className="invalid-feedback">
+//                     Please enter a valid email address.
+//                 </div>
+//             </div>
+
+//             <div className="mb-3">
+//                 <label htmlFor="message" className="form-label">
+//                     Message
+//                 </label>
+//                 <textarea
+//                     className="form-control"
+//                     id="message"
+//                     name="message"
+//                     rows="4"
+//                     required
+//                 ></textarea>
+//                 <div className="invalid-feedback">Please enter a message.</div>
+//             </div>
+
+//             <button type="submit" className="btn btn-primary">
+//                 Submit
+//             </button>
+//             {submitted && <div className="confirmation-message">Thank you for your submission!</div>}
+//         </form>
+//     );
+// }
+
+// export default Contact;
+
+
+import React, { useState } from "react";
+import "../assets/css/style.css";
 
 function Contact() {
-  return (
-    <form className="row g-3">
-      <div className="col-md-4">
-        <label htmlFor="validationDefault01" className="form-label">First name</label>
-        <input type="text" className="form-control" id="validationDefault01" defaultValue="" required />
-      </div>
-      <div className="col-md-4">
-        <label htmlFor="validationDefault02" className="form-label">Last name</label>
-        <input type="text" className="form-control" id="validationDefault02" defaultValue="" required />
-      </div>
-      <div className="col-12">
-        <button className="btn btn-primary" type="submit">Submit form</button>
-      </div>
-    </form>
-  );
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const firstName = formData.get('first-name');
+        const lastName = formData.get('last-name');
+        const email = formData.get('email');
+        const message = formData.get('message');
+
+        setSubmitted(true);
+    };
+
+    return (
+        <form id="contact-form" action="/submit-form" method="post">
+            <div className="mb-3">
+                <label htmlFor="first-name" className="form-label">
+                    First Name
+                </label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="first-name"
+                    name="first-name"
+                    required
+                />
+                <div className="invalid-feedback">Please enter your first name.</div>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="last-name" className="form-label">
+                    Last Name
+                </label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="last-name"
+                    name="last-name"
+                    required
+                />
+                <div className="invalid-feedback">Please enter your last name.</div>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                    Email Address
+                </label>
+                <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    required
+                />
+                <div className="invalid-feedback">
+                    Please enter a valid email address.
+                </div>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="message" className="form-label">
+                    Message
+                </label>
+                <textarea
+                    className="form-control"
+                    id="message"
+                    name="message"
+                    rows="4"
+                    required
+                ></textarea>
+                <div className="invalid-feedback">Please enter a message.</div>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+                Submit
+            </button>
+            {submitted && <div className="confirmation-message">Thank you for your submission!</div>}
+        </form>
+    );
 }
 
 export default Contact;
